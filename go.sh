2a0e0de1bc00031c8383ef137bf03a6bf720339b
Jean D'Amore <jdamore@thoughtworks.com>
2016-04-16 15:28:03 +1000

    Added help to go.sh

--- go.sh
+++ go.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-init() {
-	export PATH="./node_modules/.bin:$PATH" 
-	rm -rf node_modules 
-	npm install
-}
-
 build() {
 	webpack -p --progress --colors --config webpack.js
 	rm -f bundle.js
 }
 
-test() {	
-	mocha --reporter nyan --compilers js:babel-core/register,css:test/nocss-compiler.js --require test/dom.js test/**/test.js
+help() {
+	echo "Usage: go.sh [options]"
+	echo "--------"
+	echo "Options:"
+	echo "--------"
+	echo "build		build the app"
+	echo "help		this help menu"
+	echo "init		setup environment"
+	echo "preflight	before committing"
+	echo "publish		publish to NPM registry"
+	echo "test		run all tests"
 }
 
-publish() {	
-	npm publish
+init() {
+	export PATH="./node_modules/.bin:$PATH" 
+	rm -rf node_modules 
+	npm install
 }
 
 preflight() 
@@ -26,12 +31,23 @@ preflight()
 	test
 }
 
+publish() {	
+	npm publish
+}
+
+test() {	
+	mocha --reporter nyan --compilers js:babel-core/register,css:test/nocss-compiler.js --require test/dom.js test/**/test.js
+}
+
 if [ $# -eq 0 ]; then
 	preflight
-elif ([ $1 == "all" ] \
-	|| [ $1 == "init" ] \
-	|| [ $1 == "build" ] \
-	|| [ $1 == "test" ] \
-	|| [ $1 == "publish" ]); then
+elif ([ $1 == "build" 		] \
+	||  [ $1 == "help"  		] \
+	||  [ $1 == "init"  		] \
+	||  [ $1 == "preflight" ] \
+	||  [ $1 == "publish" 	] \
+	||  [ $1 == "test" 			]); then
 	$1
+else
+	echo "Usage: go.sh [options] (type 'go.sh help' for more details)"
 fi
